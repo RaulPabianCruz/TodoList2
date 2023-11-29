e7a271725302c7cdae8f84c76e8c3d04a9f15cc6
RaulPabianCruz <ito.pabian@gmail.com>
2023-11-29 14:52:03 -0800

    Add more functions to the projectManager IIFE
    Adds more functions to the projectManager module function in order to
    allow it to manipulate and retrieve the todos in each project. Also Adds
    some statements to test out the functions, but these are temporary.

--- src/index.js
+++ src/index.js
@@ -40,9 +40,80 @@ const projectManager = (function (){
         return projectArray.map((project) => project);
     }
 
-    return { addProject, deleteProject, editProject, getProjectAt, getProjectList };
+    const addTodo = function(projectIndex, todo) {
+        if(_isValidIndex(projectIndex))
+            projectArray[projectIndex].addTodo(todo);
+    }
+    const deleteTodo = function(projectIndex, todoIndex) {
+        if(_isValidIndex(projectIndex))
+            projectArray[projectIndex].deleteTodo(todoIndex);
+    }
+    const getTodoAt = function(projectIndex, todoIndex) {
+        if(_isValidIndex(projectIndex))
+            return projectArray[projectIndex].getTodoAt(todoIndex);
+        else
+            return null;
+    }
+    const getProjectTodoList = function(projectIndex) {
+        if(_isValidIndex(projectIndex))
+            return projectArray[projectIndex].getTodoList();
+        else
+            null;
+    }
+
+    return { addProject, deleteProject, editProject, getProjectAt, getProjectList,
+            addTodo, deleteTodo, getTodoAt, getProjectTodoList };
 })();
 
+//might not be needed, not too sure yet
 const logicModule = (function() {
-    //will control action such as adding/editing/deleting/fetching todos;
+    let selectedProjectIndex = 0;
+
+    const _getSelectedProject = function() {
+        return projectManager.getProjectAt(selectedProjectIndex);
+    }
+
+    const setSelectedProjectIndex = function(projectIndex) {
+        if(projectManager.getProjectAt(projectIndex) != null)
+            selectedProjectIndex = projectIndex;
+    }
+    const getSelectedProjectIndex = function() {
+        return selectedProjectIndex;
+    }   
+
+    return { setSelectedProjectIndex, getSelectedProjectIndex};
 })();
+
+const todo1 = todoFactory('todoTitle', 'desc', '1/1/2000', 'priority', 'notes');
+const todo2 = todoFactory('todoTitle2', 'desc2', '1/1/2000', 'priority2', 'notes2');
+const todo3 = todoFactory('todoTitle3', 'desc3', '1/1/2000', 'priority3', 'notes3');
+
+projectManager.addProject('project2');
+projectManager.addTodo(0, todo1);
+projectManager.addTodo(0, todo2);
+projectManager.addTodo(1,todo3);
+
+console.log('project List: ');
+let projList = projectManager.getProjectList();
+for(let i = 0; i < projList.length; i++)
+    console.log(projList[i].getTitle());
+
+console.log('todo list: default project');
+let todoList = projectManager.getProjectTodoList(0);
+for(let i = 0; i < todoList.length; i++){
+    console.log(todoList[i].getTitle());
+}
+
+logicModule.setSelectedProjectIndex(1);
+
+console.log('todo list: project2');
+todoList = projectManager.getProjectTodoList(1);
+for(let i = 0; i < todoList.length; i++){
+    console.log(todoList[i].getTitle());
+}
+
+projectManager.editProject(1, 'Project3 now');
+console.log('project List: ');
+projList = projectManager.getProjectList();
+for(let i = 0; i < projList.length; i++)
+    console.log(projList[i].getTitle());
